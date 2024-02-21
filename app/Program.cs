4d65a637ff33f61d8ded59fbd23d4edb75d610cc
Yoru-cyber <carlosmendez170210@gmail.com>
2024-02-21 07:10:24 -0400

    reduced answer time by fetching the currencies when initializing the app

--- app/Program.cs
+++ app/Program.cs
@@ -16,15 +16,25 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.MapGet("/v2/dolar", async () => 
+HttpClient client = new();
+WebScraper ws = new(client);
+string dolar_string = await ws.GetCurrency("https://www.bcv.org.ve/", "dolar");
+dolar_string = dolar_string.Replace(',', '.');
+float dolar = float.Parse(dolar_string);
+string euro_string = await ws.GetCurrency("https://www.bcv.org.ve/", "euro");
+euro_string = euro_string.Replace(',', '.');
+float euro = float.Parse(euro_string);
+app.MapGet("/v2/dolar", async () =>
 {
-    HttpClient client = new();
-    WebScraper ws = new(client);
-    string dolar_string = await ws.GetCurrency("https://www.bcv.org.ve/", "dolar");
-    dolar_string = dolar_string.Replace(',', '.');
-    float dolar = float.Parse(dolar_string);
+
     return dolar;
 });
+app.MapGet("/v2/euro", async () =>
+{
+
+    return euro;
+});
+
 app.UseHttpsRedirection();
 
 
--- app/obj/Debug/net8.0/DolarAPI.AssemblyInfo.cs
+++ app/obj/Debug/net8.0/DolarAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DolarAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f790952d9e5610a12a7e7b6283917af48e59e70c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ecec96eb8cf6be5344e58fdde903f332cc4b6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DolarAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DolarAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
